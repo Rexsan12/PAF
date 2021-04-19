8ad3bfc22b248f2a8ae96e4781b9a79626aed6b7
lakshika1999 <nrlakshika@gmail.com>
2021-04-19 10:33:36 +0530

    Researcher Service

---
+++ Gadget_Badget/src/com/ResearcherService.java
@@ -0,0 +1,93 @@
+package com;
+
+import model.Researcher;
+
+//for REST service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//for JSON
+import com.google.gson.*;
+
+//for XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+@Path("/Researcher")
+public class ResearcherService {
+
+	Researcher resObj = new Researcher();
+	
+	//for viewing doctors
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readDoctor() {
+		
+		return resObj.readResearcher();
+		
+	}
+	//researcherID','researcherName','email','location','category'
+	//for inserting doctors	
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertResearcher( @FormParam("researcherCode") String researcherCode,
+									@FormParam("researcherName") String researcherName,
+									  @FormParam("email") String email,
+									  @FormParam("location") String location,
+									  @FormParam("category") String category)
+									  
+		{
+				String output = resObj.insertResearcher(researcherCode, researcherName, email, location, category);
+				return output;
+		}
+		
+		//insert and view is tested using POSTMAN
+		
+		
+		//for updating doctors
+		   @PUT
+		   @Path("/")
+		   @Produces(MediaType.TEXT_PLAIN)
+		   public String updateDoctor(String resData) {
+			
+			 //Convert the input string to a JSON object
+			  JsonObject resjObj = new JsonParser().parse(resData).getAsJsonObject();
+			//researcherID','researcherName','email','location','category'
+			//Read the values from the JSON object
+			  String resID= resjObj.get("researcherID").getAsString();
+			  String resCode= resjObj.get("researcherCode").getAsString();
+			  String resName= resjObj.get("researcherName").getAsString();
+			  String resEmail= resjObj.get("email").getAsString();
+			  String resLocation= resjObj.get("location").getAsString();
+			  String resCategory= resjObj.get("category").getAsString();
+			  
+			  
+			  String output= resObj.updateResearcher(resID, resCode, resName, resEmail, resLocation, resCategory);
+			  return output;
+			  
+			  
+			 
+		  }
+		   
+		   
+		   //for deleting doctor
+		   @DELETE
+		   @Path("/")
+		   @Consumes(MediaType.APPLICATION_XML)
+		   @Produces(MediaType.TEXT_PLAIN)
+		   public String deleteResearcher(String resData)
+		   {
+		   			//Convert the input string to an XML document
+		   			Document doc = Jsoup.parse(resData, "", Parser.xmlParser());
+		   			//Read the value from the element <itemID>
+		   			String resID = doc.select("researcherID").text();
+		   			String output = resObj.deleteResearcher(resID);
+		   			return output;
+		   }
+}
---
+++ Gadget_Badget/src/model/Researcher.java
@@ -0,0 +1,199 @@
+package model;
+
+import java.sql.*;
+
+public class Researcher {
+
+	public Connection connect(){
+		Connection con = null;
+	 try
+	 {
+	       Class.forName("com.mysql.cj.jdbc.Driver");
+	       //Provide the correct details: DBServer/DBName, username, password
+	       con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget","root", "");
+	       
+	       //For testing
+	       System.out.print("Successfully connected");
+	 }
+	 catch(Exception e){
+
+	    e.printStackTrace();
+	 }
+	 return con;
+	}
+	
+public String insertResearcher(String resCode, String resName, String resEmail, String resLocation, String resCategory){
+	    
+	    String output = "";
+		
+		try{
+	         Connection con = connect();
+	         if (con == null)
+	         {
+	             return "Error while connecting to the database";
+	         }
+	        
+	        // create a prepared statement
+	        String query = " insert into researcher(researcherCode, researcherName, email, location, category)" + " values (?, ?, ?, ?, ?)";
+	        PreparedStatement preparedStmt = con.prepareStatement(query);
+
+	        // binding values
+	        //preparedStmt.setInt(1, 0);
+	        preparedStmt.setString(1, resCode);
+	        preparedStmt.setString(2, resName);
+	        preparedStmt.setString(3, resEmail);
+	        preparedStmt.setString(4, resLocation);
+	        preparedStmt.setString(5, resCategory);
+	        
+
+	        //execute the statement
+	        preparedStmt.execute();
+	        con.close();
+
+	        output = "Inserted successfully";
+
+		}catch(Exception e){
+
+	        output = "Error while inserting";
+	        System.err.println(e.getMessage());
+		}
+
+		return output;
+
+	}
+	
+	
+	
+	public String readResearcher(){
+
+		String output = "";
+		try
+		{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database for reading."; 
+				}
+
+
+				// Prepare the html table to be displayed
+				output = "<table border=\"1\"><tr><th>Researcher Code</th><th>Researcher Name</th><th>Email</th><th>Location</th><th>Category</th><th>Update</th><th>Remove</th></tr>";
+				String query = "select * from researcher";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					//'researcherID','researcherName','email','location','category'
+					String researcherID = Integer.toString(rs.getInt("researcherID"));
+					String researcherCode = rs.getString("researcherCode");
+					String researcherName = rs.getString("researcherName");
+					String email = rs.getString("email");
+					String location = rs.getString("location");
+					String category = rs.getString("category");
+
+					// Add into the html table
+					output += "<tr><td>" + researcherCode + "</td>";
+					output += "<td>" + researcherName + "</td>";
+					output += "<td>" + email + "</td>";
+					output += "<td>" + location + "</td>";
+					output += "<td>" + category + "</td>";
+					
+					
+					// buttons
+					output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
+					+ "<td><form method=\"post\" action=\"index.jsp\">"
+					+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+					+ "<input name=\"itemID\" type=\"hidden\" value=\"" + researcherID
+					+ "\">" + "</form></td></tr>";
+				}
+
+				con.close();
+				// Complete the html table
+				output += "</table>";
+		}
+		catch (Exception e)
+		{
+				output = "Error while reading the values.";
+				System.err.println(e.getMessage());
+		}
+	return output;
+}
+	
+	
+	
+	
+	public String updateResearcher(String resID, String resCode, String resName, String resEmail, String resLocation, String resCategory){
+	    
+	    String output = "";
+
+	    try{
+
+	           Connection con = connect();
+	           if (con == null){
+	           return "Error while connecting to the database for updating.";
+	           }
+	           
+	           // create a prepared statement
+
+	           String query = "UPDATE researcher SET researcherCode=?,researcherName=?,email=?,location=?,category=? WHERE researcherID=?";
+	           PreparedStatement preparedStmt = con.prepareStatement(query);
+	           //'researcherName','email','location','category'
+	           preparedStmt.setString(1, resCode);
+	           preparedStmt.setString(2, resName);
+	           preparedStmt.setString(3, resEmail);
+	           preparedStmt.setString(4, resLocation);
+	           preparedStmt.setString(5, resCategory);
+	           preparedStmt.setInt(6, Integer.parseInt(resID));
+
+	           // execute the statement
+	           preparedStmt.execute();
+	           con.close();
+
+	           output = "Updated successfully";
+	           
+
+
+	        }catch(Exception e){
+	        	output = "Error while updating the value.";
+				System.err.println(e.getMessage());
+	        }
+
+	        return output;
+
+	    }
+	
+	
+	public String deleteResearcher(String resID){
+
+		String output = "";
+
+		try{
+
+			Connection con = connect();
+			if (con == null){
+
+	         return "Error while connecting to the database for deleting.";
+	        }
+
+	        // create a prepared statement
+	        String query = "delete from researcher where researcherID=?";
+	        PreparedStatement preparedStmt = con.prepareStatement(query);
+	        // binding values
+	        preparedStmt.setInt(1, Integer.parseInt(resID));
+	        // execute the statement
+	        preparedStmt.execute();
+	        con.close();
+
+	        output = "Deleted successfully";
+
+
+		}catch (Exception e){
+
+			output = "Error while deleting the value.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+}
